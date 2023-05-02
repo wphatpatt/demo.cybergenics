fde9f40252301cb8114c475ea7420d020156431a
Pattarajit Wongkrung <pattarajit.w@insightera.co.th>
2023-05-02 14:48:32 +0700

    edit config

--- frontend.sh
+++
@@ -1,5 +0,0 @@
-npm install && npm run build
-#cd server && rm -rf public && mkdir public && cd ..
-#mv dist/* server/public/
-#cp entrypoint.sh server/entrypoint.sh && cp .env.staging.local server/.env.staging.local
-docker build -t cybergenics:latest .
